c8e1b66e2e7ae4a699b2be713d3a736544cd0825
ROANVL <romanychev@tut.by>
2023-09-19 02:52:26 +0300

    save changes

--- website/admin.py
+++ website/admin.py
@@ -2,27 +2,75 @@ from django.contrib import admin
 from .models import Contacts, OrderStatus, Contacts, Companies, Managers, Orders, OrderStatus, Leads, JobPosition, Department, Product
 
 
+@admin.register(Contacts)
 class ContactsAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "company",
-                    "job_title", "phone", "email", "description", "created_at"]
-    search_fields = ["first_name", "last_name", "company",
-                     "job_title", "phone", "email", "description", "created_at"]
+    list_display = ("first_name", "last_name", "company",
+                    "job_title", "phone", "email", "description", "created_at")
+    search_fields = ("first_name", "last_name", "company",
+                     "job_title", "phone", "email", "description", "created_at")
     list_per_page = 10
 
 
-admin.site.register(Contacts)
+@admin.register(Companies)
+class CompaniesAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "email", "city", "state", "zipcode",
+                    "industry", "website", "year_founded", "number_of_employees", "manager")
+    search_fields = ("name", "phone", "email", "city", "state", "zipcode", "industry", "website",
+                     "year_founded", "number_of_employees", "manager__first_name", "manager__last_name")
+    list_per_page = 10
 
 
-admin.site.register(Companies)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "quantity", "price_per_unit",
+                    "minimum_stock_level")
+    search_fields = ("name",)
+    list_per_page = 10
 
 
-admin.site.register(Product)
-admin.site.register(OrderStatus)
-admin.site.register(Orders)
+@admin.register(OrderStatus)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+    list_per_page = 10
 
 
-admin.site.register(Managers)
-admin.site.register(JobPosition)
-admin.site.register(Department)
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ("order_status", "order_id", "company", "product", "quantity",
+                    "shipping_date", "shipping_address", "order_amount", "manager", "created_at")
+    search_fields = ("order_id", "company__name", "product__name",
+                     "manager__first_name", "manager__last_name")
+    list_per_page = 10
 
-admin.site.register(Leads)
+
+@admin.register(Managers)
+class ManagersAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone",
+                    "email", "job_title", "department")
+    search_fields = ("first_name", "last_name", "phone",
+                     "email", "job_title__name", "department__name")
+    list_per_page = 10
+
+
+@admin.register(JobPosition)
+class JobPositionAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+    list_per_page = 10
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_per_page = 10
+
+
+@admin.register(Leads)
+class LeadsAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone", "email",
+                    "creation_date", "status", "lead_source", "expected_close_date")
+    search_fields = ("first_name", "last_name", "phone",
+                     "email", "status", "lead_source")
+    list_per_page = 10
--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Contacts, Companies, Managers, Orders, OrderStatus, Leads, JobPosition, Department, Product
+from django.forms.widgets import SelectDateWidget
 
 
 class SignUpForm(UserCreationForm):
